1f7174535f1cb2a7f2aa179cc24b43df274d2268
Leo <devtip@163.com>
2017-02-04 17:08:31 +0800

    fix iOS 10 -

--- LCPaintViewDemo/ViewController.m
+++ LCPaintViewDemo/ViewController.m
@@ -27,17 +27,21 @@
     
     LCPaintView *paintView = ({
         LCPaintView *paintView = [[LCPaintView alloc] init];
+        paintView.lineWidth = 5.0f;
+        paintView.lineColor = [UIColor whiteColor];
         paintView.frame = CGRectMake(0, 0, screenSize.width, screenSize.height);
         [self.view insertSubview:paintView atIndex:0];
         self.paintView = paintView;
     });
     
-    __weak typeof(paintView) weakPaintView = paintView;
-    [NSTimer scheduledTimerWithTimeInterval:5.0f repeats:NO block:^(NSTimer * _Nonnull timer) {
-        __strong typeof(weakPaintView) strongPaintView = weakPaintView;
-        
-        strongPaintView.lineColor = [UIColor redColor];
-    }];
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 10.0f) {
+        __weak typeof(paintView) weakPaintView = paintView;
+        [NSTimer scheduledTimerWithTimeInterval:5.0f repeats:NO block:^(NSTimer * _Nonnull timer) {
+            __strong typeof(weakPaintView) strongPaintView = weakPaintView;
+            
+            strongPaintView.lineColor = [UIColor redColor];
+        }];
+    }
 }
 
 - (IBAction)onClear {
